ee54e78e8e567028bbd61e7a758a0714f5a7758e
Smadden00 <Smadden1234567890@gmail.com>
2025-12-18 17:51:07 -0500

    Adding health checks

---
+++ api.py
@@ -0,0 +1,18 @@
+from Flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+##################
+# Health Checks
+##################
+@app.get("/health")
+def health_check():
+    return jsonify(status="ok"), 200
+
+@app.get("/hello")
+def hello_test():
+    name = request.args.get("name", "World")
+    return jsonify(message=f"Hello, {name}!")
+
+if __name__ == '__main__':
+    app.run(debug=True)
