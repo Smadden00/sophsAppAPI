1d548a8a82d23df0cf1d3ac7e34966f270436028
Smadden00 <Smadden1234567890@gmail.com>
2025-12-18 19:19:00 -0500

    properly loading env

--- app/config.py
+++ app/config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
     SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{os.environ['PGUSER']}:{os.environ['PGPASSWORD']}@127.0.0.1:{os.environ['PGPORT']}/{os.environ['PGDATABASE']}"
