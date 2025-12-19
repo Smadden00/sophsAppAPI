498acb5096a01623204af5a05380cef60dd08d6c
Smadden00 <Smadden1234567890@gmail.com>
2025-12-18 19:13:04 -0500

    fixing typo

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{os.environ["PGUSER"]}:{os.environ["PGPASSWORD"]}@127.0.0.1:{os.environ["PGPORT"]}/{os.environ["PGDATABASE"]}"
+    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{os.environ['PGUSER']}:{os.environ['PGPASSWORD']}@127.0.0.1:{os.environ['PGPORT']}/{os.environ['PGDATABASE']}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # 50MB upload limit
